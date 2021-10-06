805fa58e9ef9fd7ae153d6d0b4432ebe96bd64f9
nommm <77090591+nommm@users.noreply.github.com>
2021-10-06 09:19:57 +0700

    Update mp3.py

--- paf/mp3.py
+++ paf/mp3.py
@@ -1,6 +1,6 @@
 import pafy
 import os
-import __main__
+
 
 if not os.path.exists('download'):
     os.makedirs('download')
@@ -33,20 +33,17 @@ audiostreams[3].download("download")
 
 os.chdir('download')
 
-filemp3 = video.title + ".webm" or '.m4a'
-if filemp3 == video.title + ".mp3":
-    print("pass")
-else:
-    print('no')
+filemp3 = video.title + ".webm"
+filempp3 = video.title + ".m4a"
+
+if filemp3 == video.title + ".webm" or filempp3 == video.title + ".m4a" :
+    print("no")
     base = os.path.splitext(filemp3)[0]
-    os.rename(filemp3, base + '.mp3')
+    os.rename(filemp3 or filempp3, base + '.mp3')
+
 
 clearConsole()
 
 print("\033[1m" + video.title + "\033[0m")
 print(video.author)
 print('\nSucess...\n')
-
-input("Press Enter to continue...")
-
-__main__()

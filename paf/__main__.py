1e061a16f931971879b4220b350c3548889c0bf0
nommm <77090591+nommm@users.noreply.github.com>
2021-10-06 09:32:44 +0700

    Update __main__.py

--- paf/__main__.py
+++ paf/__main__.py
@@ -6,30 +6,7 @@ def clearConsole():
         command = 'cls'
     os.system(command)
 
-def tite():
-    print("\033[1m" + "Video[0]" + "\033[0m")
-    print("\033[1m" + "Audio[1]" + "\033[0m")
 
 clearConsole()
 
-
-tite()
-
-while True:
-    x = input('> ')
-    if x == "0":
-
-        import mp4 
-        break
-
-    elif x == "1":
-
-        import mp3
-        break
-
-    else :
-
-        clearConsole()
-        tite()
-
-        
+import Run
